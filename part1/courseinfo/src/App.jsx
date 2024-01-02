b6b246a5b8f25342fd8eb5204fb142d2c2164224
zestx5 <buyankov.ilya@gmail.com>
2024-01-02 15:36:58 +0300

    refactor course info into objects

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,7 +6,7 @@ const Content = ({ info }) => {
   return (
     <div>
       {info.map((p) => (
-        <Part part={p.part} exercises={p.exercises} />
+        <Part part={p.name} exercises={p.exercises} />
       ))}
     </div>
   );
@@ -26,24 +26,24 @@ const Total = ({ sum }) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        info={[
-          { part: part1, exercises: exercises1 },
-          { part: part2, exercises: exercises2 },
-          { part: part3, exercises: exercises3 },
-        ]}
-      />
-      <Total sum={exercises1 + exercises2 + exercises3} />
+      <Content info={[part1, part2, part3]} />
+      <Total sum={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
