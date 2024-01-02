a77341c565d9176364cbcb82eeb7e3cd30a58685
zestx5 <buyankov.ilya@gmail.com>
2024-01-02 15:39:56 +0300

    refactor: use array for info

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -26,24 +26,26 @@ const Total = ({ sum }) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content info={[part1, part2, part3]} />
-      <Total sum={part1.exercises + part2.exercises + part3.exercises} />
+      <Content info={parts} />
+      <Total sum={parts.reduce((prev, cur) => (prev += cur.exercises), 0)} />
     </div>
   );
 };
