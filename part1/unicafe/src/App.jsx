a30e164ad043bf2596c66a46815fbb27921e589e
zestx5 <buyankov.ilya@gmail.com>
2024-01-03 15:02:40 +0300

    add: positive and avg

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [all, setAll] = useState(good + bad + neutral);
+  const [all, setAll] = useState(0);
 
   const onClickUpdateFeedback = (value) => {
     switch (value) {
@@ -45,6 +45,8 @@ const App = () => {
       <Paragraph value={`neutral ${neutral}`} />
       <Paragraph value={`bad ${bad}`} />
       <Paragraph value={`all ${all}`} />
+      <Paragraph value={`average ${(good - bad) / all || 0}`} />
+      <Paragraph value={`positive ${(good * 100) / all || 0}%`} />
     </>
   );
 };
