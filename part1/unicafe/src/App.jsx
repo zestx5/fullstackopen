93d0806b5d3baa6ec0e10ca4ab37bdfc0e88f887
zestx5 <buyankov.ilya@gmail.com>
2024-01-03 15:07:10 +0300

    refactor: statistics into component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,6 +4,20 @@ const Heading = ({ value }) => {
   return <h1>{value}</h1>;
 };
 
+const Statistics = ({ good, neutral, bad }) => {
+  let all = good + bad + neutral;
+  return (
+    <>
+      <Paragraph value={`good ${good}`} />
+      <Paragraph value={`neutral ${neutral}`} />
+      <Paragraph value={`bad ${bad}`} />
+      <Paragraph value={`all ${all}`} />
+      <Paragraph value={`average ${(good - bad) / all || 0}`} />
+      <Paragraph value={`positive ${(good * 100) / all || 0}%`} />
+    </>
+  );
+};
+
 const Button = ({ value, onClickHandler }) => {
   return <button onClick={() => onClickHandler(value)}>{value}</button>;
 };
@@ -16,21 +30,17 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [all, setAll] = useState(0);
 
   const onClickUpdateFeedback = (value) => {
     switch (value) {
       case "good":
         setGood(good + 1);
-        setAll(all + 1);
         break;
       case "neutral":
         setNeutral(neutral + 1);
-        setAll(all + 1);
         break;
       case "bad":
         setBad(bad + 1);
-        setAll(all + 1);
     }
   };
 
@@ -41,12 +51,7 @@ const App = () => {
       <Button value={"neutral"} onClickHandler={onClickUpdateFeedback} />
       <Button value={"bad"} onClickHandler={onClickUpdateFeedback} />
       <Heading value={"statistics"} />
-      <Paragraph value={`good ${good}`} />
-      <Paragraph value={`neutral ${neutral}`} />
-      <Paragraph value={`bad ${bad}`} />
-      <Paragraph value={`all ${all}`} />
-      <Paragraph value={`average ${(good - bad) / all || 0}`} />
-      <Paragraph value={`positive ${(good * 100) / all || 0}%`} />
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </>
   );
 };
