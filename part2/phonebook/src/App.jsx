2d64693d04574f80fe24062962da8f6029b175ea
zestx5 <buyankov.ilya@gmail.com>
2024-01-05 13:54:01 +0300

    add: data fetched from json-server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import Form from "./components/Form/Form";
 import Numbers from "./components/Numbers/Numbers";
 import Filter from "./components/Filter/Filter";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
   const [personsToShow, setPersonsToShow] = useState(persons);
+
+  const fetchPersonData = () => {
+    axios.get("http://localhost:3001/persons").then((d) => {
+      setPersons(d.data);
+      setPersonsToShow(d.data);
+    });
+  };
+
+  useEffect(fetchPersonData, []);
 
   const onChangeSetNewName = (e) => {
     setNewName(e.target.value);
